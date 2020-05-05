4f0c75330bd7a46cb54aa96e666086ccf156cda4
arthur <amackeith@uchicago.edu>
2020-05-04 23:10:10 -0500

    small

---
+++ launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+echo "This is an empty launch script. Update it to launch your application."
+
+rosrun joy joy_node
