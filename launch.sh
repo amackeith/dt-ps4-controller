60f1ae0ede2f956d3b3f38dfe16f462d48fc710c
arthur <amackeith@uchicago.edu>
2020-05-04 23:25:04 -0500

    sm

--- launch.sh
+++ launch.sh
@@ -5,5 +5,5 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 echo "This is an empty launch script. Update it to launch your application."
-
+export ROS_NAMESPACE=/drone2
 rosrun joy joy_node
